a19426cdb7f085a70363d8b9114488bb9e763e63
artem.stepanov <artemstepanov0114@gmail.com>
2026-06-27 22:42:52 +0700

    feat(ui): add Label component skeleton

---
+++ app/components/ui/Label.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="label">
+		<div v-if="icon" class="label__icon">
+			<Icon :name="icon"/>
+		</div>
+		<div class="label__value">{{ value }}</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+	type TComponentProps = {
+		icon?: string;
+		value: string;
+	};
+
+	defineProps<TComponentProps>();
+</script>
