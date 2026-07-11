48a202eb207f35c1f542a097c6d1cdcceed2a6e9
artem.stepanov <artemstepanov0114@gmail.com>
2026-07-11 12:44:22 +0700

    revert(fonts): remove temporary font switcher

--- app/app.vue
+++ app/app.vue
@@ -3,13 +3,11 @@
 		<Header/>
 		<NuxtPage/>
 		<Footer/>
-		<DevFontSwitcher/>
 		<DevLanguageSwitcher/>
 	</div>
 </template>
 <script setup lang="ts">
 	import Footer from "~/components/layout/Footer.vue";
 	import Header from "~/components/layout/Header.vue";
-	import DevFontSwitcher from "~/components/dev/FontSwitcher.vue";
 	import DevLanguageSwitcher from "~/components/dev/LanguageSwitcher.vue";
 </script>
